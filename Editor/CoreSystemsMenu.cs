df907684f7023a23f0680b584610e4eeac08c422
NekoD <huangqiongtong@gmail.com>
2025-11-04 14:39:43 +0800

    feat: 添加SystemMenu中的Gameproperty窗口

---
+++ Editor/CoreSystemsMenu.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+
+namespace EasyPack.Editor
+{
+    /// <summary>
+    /// 核心系统菜单
+    /// </summary>
+    public static class CoreSystemsMenu
+    {
+        [MenuItem("EasyPack/CoreSystems/游戏属性(GameProperty)/管理器窗口")]
+        public static void OpenGamePropertyManagerWindow()
+        {
+            var window = EditorWindow.GetWindow<GamePropertyManagerWindow>("GameProperty Manager");
+            window.Show();
+        }
+    }
+}
