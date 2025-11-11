1d9b6dc3e9d4b0554a5a56cf27ef611e15ce7da7
一冰在北 <wu2687180662@icloud.com>
2025-11-11 20:15:43 +0800

    opt: 修改Pump方法默认最大事件数为Int32.MaxValue

--- 04_GameplaySystems/EmeCard/Runtime/CardEngine.cs
+++ 04_GameplaySystems/EmeCard/Runtime/CardEngine.cs
@@ -79,7 +79,7 @@ namespace EasyPack.EmeCardSystem
         /// 事件主循环，依次处理队列中的所有事件。
         /// </summary>
         /// <param name="maxEvents">最大处理事件数。</param>
-        public void Pump(int maxEvents = 2048)
+        public void Pump(int maxEvents = Int32.MaxValue)
         {
             if (_isPumping) return;
             _isPumping = true;
