58e42756a0301c35830025c8f126611abb467948
一冰在北 <wu2687180662@icloud.com>
2026-07-07 16:47:06 +0800

    style:修改一个警告日志的位置

--- Modules/EmeCard/Runtime/Services/CardEngine.Cache.cs
+++ Modules/EmeCard/Runtime/Services/CardEngine.Cache.cs
@@ -254,9 +254,9 @@ namespace EasyPack.EmeCardSystem
 
             if (_cardsByPosition.TryGetValue(newPosition, out Card existingCard) && !existingCard.Equals(card))
             {
-                Debug.LogWarning($"[CardEngine] 位置 {newPosition} 已被占用，无法移动卡牌 '{card.Id}' (UID: {card.UID})");
                 if (!forceOverwrite)
                 {
+                    Debug.LogWarning($"[CardEngine] 位置 {newPosition} 已被占用，无法移动卡牌 '{card.Id}' (UID: {card.UID})");
                     return false;
                 }
                 
